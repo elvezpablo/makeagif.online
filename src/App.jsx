397de4b32c55e24576433cdce1716f2ad8d4ec61
elvezpablo <prangel@gmail.com>
2020-11-13 07:18:48 -0800

    import mvp

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 import './App.css';
 
+// https://engineering.giphy.com/how-to-make-gifs-with-ffmpeg/
+// https://stackoverflow.com/questions/15464896/get-cpu-gpu-memory-information
+// https://stackoverflow.com/questions/45661913/vanilla-javascript-preview-video-file-before-upload-no-jquery
+// https://github.com/ffmpegwasm/ffmpeg.wasm/blob/master/docs/api.md
+
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
+  const [videoSrc, setVideoSrc] = useState();
+  const [outputSrc, setOutputSrc] = useState();
+  // const [message, setMessage] = useState('Click Start to transcode');
+  const ffmpeg = createFFmpeg({
+    log: true,
+  });
+  const setInputVideo = ({ target: { files } }) => {
+    console.log(files);
+    // TODO: warn users of 2 GB file limit
+
+    if (files && files.length) {
+      setVideoSrc(files[0])
+    }
+
+  }
+
+
+  const filter = "[0:v] fps=12,scale=w=480:h=-1,split [a][b];[a] palettegen [p];[b][p] paletteuse";
+
+  const doTranscode = async () => {
+    console.log("doTranscode");
+    console.log('Loading ffmpeg-core.js');
+    await ffmpeg.load();
+    const inputName = videoSrc?.name || "";
+    console.log('Start transcoding: ', inputName);
+    if (videoSrc) {
+
+      ffmpeg.FS('writeFile', inputName, await fetchFile(videoSrc));
+    }
+
+    await ffmpeg.run('-i', inputName, '-filter_complex', filter, 'output.gif');
+    console.log('Complete transcoding');
+    const data = ffmpeg.FS('readFile', 'output.gif');
+    setOutputSrc(URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' })));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      {/* <video src={ } /> */}
+      <img src={outputSrc} />
+      <input type="file" onChange={setInputVideo} />
+      <button onClick={doTranscode}>Start</button>
     </div>
   );
 }
